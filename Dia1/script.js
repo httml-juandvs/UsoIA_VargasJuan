bd7075d20b24930d35cb0a6f10baac1a8f093be2
Juan Da <juandavargassoto@gmail.com>
2025-12-04 17:32:33 -0500

    Feat: ✨ Personajes Rick y Morty

---
+++ Dia1/script.js
@@ -0,0 +1,41 @@
+const input = document.getElementById("id1");
+const button = document.querySelector(".bt1");
+const container = document.querySelector(".container-cards");
+
+button.addEventListener("click", () => {
+    const name = input.value.trim();
+
+    if (name === "") {
+        alert("Ingrese un nombre del personaje");
+        return;
+    }
+
+    fetch(`https://rickandmortyapi.com/api/character/?name=${name}`)
+        .then(res => res.json())
+        .then(data => {
+            mostrarPersonajes(data.results);
+        })
+        .catch(() => {
+            container.innerHTML = "";
+        });
+});
+
+
+function mostrarPersonajes(lista) {
+    container.innerHTML = "";
+
+    lista.forEach(p => {
+        const card = `
+            <div class="cont-character">
+                <div class="pict-character">
+                    <img src="${p.image}" alt="${p.name}">
+                </div>
+                <div class="info-character">
+                    <h2>Nombre: ${p.name}</h2>
+                    <p>Estado: ${p.status}</p>
+                </div>
+            </div>
+        `;
+        container.innerHTML += card;
+    });
+}
